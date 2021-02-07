baf14833d90662d35d4a0dd18a14f0a43eae948b
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-02-07 03:14:32 -0800

    Update sample_config.py

--- SaitamaRobot/sample_config.py
+++ SaitamaRobot/sample_config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     API_ID = 1383845  # integer value, dont use ""
     API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
-    TOKEN = "1428003210:AAGaq_PphUbLpNof_f5_2Cks16AORUEnpag"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "1428003210:AAHjY4AVmJTBqcOYz0bqe9_Ih4psGcwW-AU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1131653685  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "kavinduaj"
     SUPPORT_CHAT = "lkhitech"  # Your own group for support, do not add the @
