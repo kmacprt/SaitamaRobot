0f22f5159575c0716af75e9039b882df087493ea
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-02-07 03:14:02 -0800

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -3,7 +3,7 @@ from SaitamaRobot.sample_config import Config
 class Development(Config):
     OWNER_ID = 1131653685  # your telegram ID
     OWNER_USERNAME = "kavinduaj"  # your telegram username
-    API_KEY = "1428003210:AAGaq_PphUbLpNof_f5_2Cks16AORUEnpag"  # your api key, as provided by the @botfather
+    API_KEY = "1428003210:AAHjY4AVmJTBqcOYz0bqe9_Ih4psGcwW-AU"  # your api key, as provided by the @botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://kaj12:dkkajcmd@0951357@postgresql/postgres'  # sample db credentials
     JOIN_LOGGER = '-1001329348574' # some group chat that your bot is a member of
     USE_JOIN_LOGGER = True
